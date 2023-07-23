993c168b137d9a07897f7f3d199197048b8f99d5
Dogo <47029530+dogoterra@users.noreply.github.com>
2023-07-23 14:00:37 +0200

    [Release] First version

---
+++ dni.js
@@ -0,0 +1,31 @@
+const dniInput = document.getElementById('dniInput')
+const dniButton = document.getElementById('dniButton')
+dniInput.addEventListener('keypress', (e) => {
+    if (e.key==='Enter') dniButton.click()
+})
+
+esDNI = (dni) => {
+    if (!/^[0-9]{8}[A-Z]$/.test(dni)) {
+        return false;
+    } else {
+        const letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+
+        // Extraer número y letra del DNI
+        const numero = dni.substr(0, dni.length - 1);
+        const letraDNI = dni.substr(dni.length - 1);
+
+        // Calcular letra esperada
+        const letraCalculada = letras[numero % 23]
+
+        return letraDNI === letraCalculada
+    }
+}
+
+validarDNI = () => {
+    const dni = dniInput.value.trim().toUpperCase()
+
+    if (esDNI(dni))
+        document.getElementById('resultado').textContent = `El DNI ${dni} es válido.`
+    else
+        document.getElementById('resultado').textContent = `El DNI ${dni} no es válido.`
+}
